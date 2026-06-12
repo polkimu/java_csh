f3e7bd067a86539ae8dff52d6ea89bff34509c48
Choi SeungHyoek <kwona6791@gmail.com>
2026-06-12 11:24:35 +0900

    2026 06 12 폴더명 수정

--- workspace/02_oop/ch14/StringTest.java
+++ workspace/02_oop/ch14/StringTest.java
@@ -8,14 +8,16 @@ public class StringTest {
         System.out.println(s1 == s2);
         System.out.println(s1 == "Hello");
         System.out.println(s2 == "Hello");
+        //실제 값이 아닌 주소를 비교
 
         System.out.println(s1.equals(s2));
         System.out.println(s1.equals("Hello"));
         System.out.println(s2.equals("Hello"));
+        //실제 값을 비교
 
         s1 += " World";
         s1 += " Java";
-        s1 += " Program";
+        s1 += " Program"; // heap 메모리 영역에 String 객체가 계속해서 생성됨 (String객체는 불변하기 때문)
 
         System.out.println(s1);
     }
--- workspace/NewHelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class NewHelloWorld {
-    void main(){
-        System.out.println("New Hello World!!!");
-    }
-}
